20886d74658b48a17c72364cdb5257290393ff09
aavirbhav2025 <aavirbhav2025@gmail.com>
2025-09-07 08:56:41 +0530

    addd

--- assets/forms/register.php
+++ assets/forms/register.php
@@ -2,6 +2,11 @@
 session_start();
 include 'db.php'; // Your DB connection file
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim($_POST['username']);
     $phone = trim($_POST['phone']);
